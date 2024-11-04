ffa05f9409b071d26c1d747f2efbc85b357aae21
KeithDang1610 <167521177+KeithDang1610@users.noreply.github.com>
2024-11-04 13:02:47 +0700

    add the folder

--- app.py
+++ app.py
@@ -19,6 +19,5 @@ def predict():
     output = round(prediction[0],4)
     return render_template("index.html",prediction_text="crew should be{}".format(output))
     
-
 if __name__ == '__main__':
-    app.run(port = 5000, debug=True)
+    app.run(host = "0.0.0.0", port = 8080, debug=True)
